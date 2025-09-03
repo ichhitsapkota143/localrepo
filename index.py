049684c9825e06bbb883ddcb90ebe97fde8c10e1
ichhitSapkota <sapkotaichhit14@gmail.com>
2025-09-03 20:46:50 +0545

    feat: New line of code added

--- index.py
+++ index.py
@@ -1,3 +1,6 @@
 print('This is LocalRepo/index.py')
 # --- IGNORE ---
 print('This line is to be ignored')
+print('This line is to be included')
+# --- END IGNORE ---
+print('End of LocalRepo/index.py')
